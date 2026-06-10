c42c7b00ffc821669983890a2f46c6fe22e97a6b
Omar Alikhan <oms.alikhan@gmail.com>
2026-06-10 19:31:02 +0300

    Completed exercise 2.12 - 2.15 phonebook

--- part02/phonebook/src/components/Persons.jsx
+++ part02/phonebook/src/components/Persons.jsx
@@ -1,11 +1,12 @@
-const Persons = ({persons, filter}) => {
+const Persons = ({ persons, filter, handleDeletePerson }) => {
   return (
     <>
       {persons
-        .filter((p) => p.name.includes(filter))
-        .map((p) => (
-          <div key={p.name}>
+        .filter(p => p.name.includes(filter))
+        .map(p => (
+          <div id={p.id} key={p.name}>
             {p.name} {p.number}
+            <button onClick={() => handleDeletePerson(p)}>delete</button>
           </div>
         ))}
     </>
